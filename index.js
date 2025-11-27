1907f7b64771e2d3a3ba4b3c6652141e5633c0f7
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-11-27 12:16:52 +0000

    input fields added

---
+++ index.js
@@ -0,0 +1,8 @@
+Document.addEventListener('DOMContentLoaded', () => {
+    const calculateBtn = document.getElementById('calculateBtn');
+
+    calculateBtn.addEventListener('click', () => {
+       // test listner 
+        console.log('Calculate button clicked');
+    });
+    });
